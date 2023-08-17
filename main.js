038d61d5375e75ac7a8cf30cb87767360cf18f87
amine27ou <amineou433@gmail.com>
2023-08-17 21:06:37 +0100

    auto

---
+++ main.js
@@ -0,0 +1,28 @@
+const submitBtn = document.querySelector("#sub");
+const inputs = document.querySelectorAll("form input");
+
+submitBtn.addEventListener("click",function(e){
+    e.preventDefault();
+    inputs.forEach(function(input){
+        if(!input.value){
+            input.parentElement.classList.add("error");
+            console.log(input.parentElement)
+        }
+        else{
+            input.parentElement.classList.remove("error");
+            if(input.type=='email'){
+                if(validateEmail(input.value)){
+                    input.parentElement.classList.remove("error");
+                }
+                else{
+                    input.parentElement.classList.add("error");
+                }
+            }
+        }
+    })
+})
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
